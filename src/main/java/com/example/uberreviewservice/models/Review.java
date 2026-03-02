9b0cf17b2c300419d1e63e7eb2d559b93ad4eac6
“Vedant <vgavate3011@gmail.com>
2026-03-02 23:33:50 +0530

    Added more column in Review table

--- src/main/java/com/example/uberreviewservice/models/Review.java
+++ src/main/java/com/example/uberreviewservice/models/Review.java
@@ -1,10 +1,37 @@
 package com.example.uberreviewservice.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.hibernate.annotations.TenantId;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
-@Entity
+import java.util.Date;
+
+@Entity //work on java logic layer
+@Table(name = "BookingReview") //work on the database logic layer
+
 public class Review {
     @Id //this annotation makes the id property as a primary key of our table
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id ;
+
+    @Column(nullable = false) //this will not change once the column is created bcz  in properties we have given update
+    String content;
+
+    Double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP) //This annotation tells spring about the formats of date object to be started i.e. Date /Time /Timestamp
+    @CreatedDate  //this annotation tells spring that only handle it for object creation
+    Date createdAt;
+
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate  // this annotation tells spring that only handle it for object update
+    Date updatedAt;
+
 }
+
+
+//New Review(content,rating, createdAt,updatedAt)
