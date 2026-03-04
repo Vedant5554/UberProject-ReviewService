a971a8eedc62f8b96294cd37a2a32f8388d39e0c
“Vedant <vgavate3011@gmail.com>
2026-03-04 19:14:48 +0530

    Configuring UUIDs as Primary Key

--- src/main/java/com/example/uberreviewservice/repositories/ReviewRepository.java
+++ src/main/java/com/example/uberreviewservice/repositories/ReviewRepository.java
@@ -6,7 +6,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+public interface ReviewRepository extends JpaRepository<Review, UUID> {
 
 }
