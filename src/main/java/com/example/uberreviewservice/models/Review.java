22bf3325cc756c94560f9652cd398b7c615516b0
“Vedant <vgavate3011@gmail.com>
2026-03-06 16:08:20 +0530

    Added @MappedSuperclass

--- src/main/java/com/example/uberreviewservice/models/Review.java
+++ src/main/java/com/example/uberreviewservice/models/Review.java
@@ -14,35 +14,22 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
 @Entity //work on java logic layer
 @Table(name = "BookingReview") //work on the database logic layer
 
-public class Review {
-    @Id //this annotation makes the id property as a primary key of our table
-    @GeneratedValue(generator = "UUID")
-    private UUID id ;
+public class Review extends BaseModel{
+
 
     @Column(nullable = false) //this will not change once the column is created bcz  in properties we have given update
     private String content;
 
     private Double rating;
 
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP) //This annotation tells spring about the formats of date object to be started i.e. Date /Time /Timestamp
-    @CreatedDate  //this annotation tells spring that only handle it for object creation
-    private Date createdAt;
 
-
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @LastModifiedDate  // this annotation tells spring that only handle it for object update
-    private Date updatedAt;
 
     @Override
     public String toString() {
-        return "Review: " + this.content + " " + this.rating + " " + this.createdAt;
-    }
+        return "Review: " + this.content + " " + this.rating + " " + this.createdAt; }
 }
 
 
