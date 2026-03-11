29d73615de7e056ef8ef5e0f7c76a6fbaf59380c
“Vedant <vgavate3011@gmail.com>
2026-03-11 22:08:36 +0530

    about N+1 problem

--- src/main/java/com/example/uberreviewservice/repositories/BookingRepository.java
+++ src/main/java/com/example/uberreviewservice/repositories/BookingRepository.java
@@ -1,6 +1,7 @@
 package com.example.uberreviewservice.repositories;
 
 import com.example.uberreviewservice.models.Booking;
+import com.example.uberreviewservice.models.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,6 @@ import java.util.UUID;
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
     List<Booking> findAllByDriverId(Long driverid);
+
+    List<Booking> findAllByDriverIn(List<Driver> drivers);
 }
--- src/main/java/com/example/uberreviewservice/repositories/DriverRepository.java
+++ src/main/java/com/example/uberreviewservice/repositories/DriverRepository.java
@@ -1,10 +1,7 @@
 package com.example.uberreviewservice.repositories;
 
-import com.example.uberreviewservice.models.Booking;
-import com.example.uberreviewservice.models.CustomDriver;
 import com.example.uberreviewservice.models.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,15 +10,6 @@ public interface DriverRepository extends JpaRepository<Driver, Long> {
 
     Optional<Driver> findByIdAndLicenseNumber(Long id , String LicenseNumber);
 
-
-    @Query(nativeQuery = true , value = "SELECT * FROM driver WHERE id = :id AND license_number =:license") //this is raw mysql query ,error is throen at runtime in query
-    Optional<Driver> rawfingByIdAndLicenseNumber(Long id, String license);
-
-//    @Query("SELECT d FROM Driver d WHERE d.id = :id AND d.licenseNumber = :ln")
-//    Optional<Driver> hqlfindByIdAndLicense(Long id , String ln);
-
-    @Query("SELECT d FROM Driver d WHERE d.id = :id AND d.licenseNumber = :ln")
-    Optional<CustomDriver> hqlfindByIdAndLicense(Long id , String ln);
-
+    List<Driver> findAllByIdIn(List<Long> driver_id);
 
 }
